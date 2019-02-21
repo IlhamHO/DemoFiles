8275a5ede4cd8e469b018a1581f6e785d46b7b94
mobapp10 <mobapp10@student.ehb.be>
2019-02-21 16:04:14 +0100

    Initial commit

---
+++ app/src/main/java/be/ehb/demofiles/OnsObject.java
@@ -0,0 +1,22 @@
+package be.ehb.demofiles;
+
+import java.io.Serializable;
+
+public class OnsObject implements Serializable {
+    private String onzeString;
+
+    public OnsObject() {
+    }
+
+    public OnsObject(String onzeString) {
+        this.onzeString = onzeString;
+    }
+
+    public String getOnzeString() {
+        return onzeString;
+    }
+
+    public void setOnzeString(String onzeString) {
+        this.onzeString = onzeString;
+    }
+}
